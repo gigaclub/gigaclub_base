eb75bb2d6792beaf0bda3267d284cf51b7f6be35
KevTVKevin <xxkevtvxx@gmail.com>
2021-05-21 22:37:49 +0200

    imp(gigaclub_base): added manifest

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'GigaClub Base',
+    'version': '14.0.1.0.0',
+    'summary': 'GigaClub Base Module',
+    'category': 'GigaClub',
+    'author': 'GigaClub',
+    'website': 'https://GigaClub.net/',
+    'license': 'MIT',
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False
+}
