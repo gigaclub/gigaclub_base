8a9f531eb51b09e187dc0aa97a87ccc1fa385b42
Kevin <kevin@gigaclub.net>
2021-06-12 20:25:32 +0200

    imp(gigaclub_base): added mc_uuid unique constraint and default for state

--- models/gc_user.py
+++ models/gc_user.py
@@ -6,4 +6,8 @@ class GCUser(models.Model):
 
     name = fields.Char()
     mc_uuid = fields.Char()
-    state = fields.Selection(selection=[("online", "Online"), ("offline", "Offline")])
+    state = fields.Selection(selection=[("online", "Online"), ("offline", "Offline")], default="offline")
+
+    _sql_constraints = [
+        ('mc_uuid_unique', 'UNIQUE(mc_uuid)', 'MC_UUID must be unique!')
+    ]
